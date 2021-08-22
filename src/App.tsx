cb65d11599e53585322670bb6a7070f0affde5fb
Matheus Gomes <gomesprogramacao@gmail.com>
2021-08-22 10:30:06 -0300

    styles: new indentation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import { ThemeProvider, DefaultTheme } from 'styled-components'
-import usePersistedState from './utils/usePersistedState'
 import Routes from './routes'
 
+// utils
+import usePersistedState from './utils/usePersistedState'
+
+// theme
 import ThemeSwitcher from './components/ThemeSwitcher'
 import light from './styles/themes/light'
 import dark from './styles/themes/dark'
 
+// styles
 import GlobalStyle from './styles/global'
 
+// -------------------------------------------------
+// Export Function
+// -------------------------------------------------
 function App(): any {
   const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', dark)
 
